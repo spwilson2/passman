1c7079bdfe816b81def1d8de55493e72a8d2ac97
Sean Wilson <sean.wilson@ghs.com>
2020-02-15 15:03:49 -0800

    Add bootstrap and example static table

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Table } from 'react-bootstrap';
+
+const tableStyle = {
+    width: '100%',
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+        <header className="App-header">
+        <Table bordered hover striped variant="dark">
+        <tr>
+            <th>Name</th>
+            <th>ID (Email, Username, Etc.)</th>
+            <th>Password</th>
+        </tr>
+        <tbody>
+            <tr>
+                <th>Sean</th>
+                <th>spwilson27@gmail.com</th>
+                <th>Secret pass</th>
+            </tr>
+            <tr>
+                <th>Sean</th>
+                <th>spwilson27@gmail.com</th>
+                <th>Secret pass</th>
+            </tr>
+        </tbody>
+        </Table>
+        </header>
+        </div>
+    );
 }
+
+
+/*
+ * TODO: Single page app.
+ */
 
 export default App;
