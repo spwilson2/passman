74f912c4a5722383a1e155cf15407498063d11e0
Sean Wilson <sean.wilson@ghs.com>
2020-02-29 11:51:24 -0800

    Unkown chagnes

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,106 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-import { Table } from 'react-bootstrap';
 
 class TableEntry {
-    constructor(public name: string, public id: string, public password: string) {}
+    private state: any;
+    constructor(name: string, id: string, password: string) {
+        this.state = {
+            "name": name,
+            "id": id,
+            "password": password,
+            "hidden": true,
+        };
+    }
+
+    renderPassword() {
+        if (this.state.hidden)
+            return ("•".repeat(8));
+        return this.state.password;
+    }
 
     render () {
         return (
             <tr>
-                <th>{this.name}</th>
-                <th>{this.id}</th>
-                <th>{this.password}</th>
+                <td>{this.state.name}</td>
+                <td>{this.state.id}</td>
+                <td>{this.renderPassword()}</td>
             </tr>
         )
     }
 }
 
+class PasswordTableComponent extends React.Component<{}, any> {
+
+    constructor(props:any) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <table className="pure-table">
+            <tr>
+                <th>Name</th>
+                <th>ID (Email, Username, Etc.)</th>
+                <th>Password</th>
+            </tr>
+            <tbody>
+                {exampleData.map(o => o.render())}
+            </tbody>
+            </table>
+        )
+    }
+}
+
 const exampleData = [
-    new TableEntry("Sean", "spwilson27@gmail.com", "super secret"),
-    new TableEntry("Sean", "spwilson27@gmail.com", "super secret"),
+    new TableEntry("Dropbox", "spwilson27@gmail.com", "super secret"),
+    new TableEntry("Dropbox", "spwilson27@gmail.com", "super secret"),
 ]
+
+//class BackendSelectionModal extends React.Component<{}, {show: boolean}> {
+//
+//    constructor(props:any) {
+//        super(props);
+//        this.handleLoad = this.handleLoad.bind(this);
+//        this.state = {show: false};
+//    }
+//
+//    componentDidMount() {
+//        window.addEventListener('load', this.handleLoad);
+//    }
+//
+//    handleLoad() {
+//        this.setState({show: false});
+//    }
+//
+//    render() {
+//        return (
+//            <Modal show={this.state.show}>
+//                <Modal.Header closeButton>
+//                    <Modal.Title>Backend Selection</Modal.Title>
+//                </Modal.Header>
+//                <Modal.Body>
+//                    <p>
+//                        Select the platform which you'd like to store your encrypted password data in.
+//                    </p>
+//                    <Button>None (Don't save data)</Button>
+//                </Modal.Body>
+//            </Modal>
+//        )
+//    }
+//}
 
 function App() {
     return (
         <div className="App">
         <header className="App-header">
-        <Table bordered hover striped variant="dark">
-        <tr>
-            <th>Name</th>
-            <th>ID (Email, Username, Etc.)</th>
-            <th>Password</th>
-        </tr>
-        <tbody>
-            {exampleData.map(o => o.render())}
-        </tbody>
-        </Table>
         </header>
+             <div className="pure-menu pure-menu-horizontal" style={{textAlign:"left"}} >
+                <a className="pure-menu-heading pure-menu-link">Passman</a>
+                <ul className="pure-menu-list">
+                </ul>
+            </div>           
+            <PasswordTableComponent />
         </div>
     );
 }
